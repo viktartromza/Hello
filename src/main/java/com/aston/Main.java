085e23eb8fc3eb9b43bcc52ab194eafa4c460a9e
Viktar Tromza <viktar.tromza@gmail.com>
2023-07-12 23:24:43 +0300

    Init Commit

---
+++ src/main/java/com/aston/Main.java
@@ -0,0 +1,11 @@
+package com.aston;
+
+import com.aston.config.SpringConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+    }
+}
---
+++ src/main/java/com/aston/config/SpringConfig.java
@@ -0,0 +1,22 @@
+package com.aston.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.aston")
+public class SpringConfig implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
